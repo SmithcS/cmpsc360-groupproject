bfd4a7b05f0ba3a90f562b380321d5d9cacc68ec
Smith Sopp <smithsopp@gmail.com>
2017-11-17 01:07:51 -0500

    added some new queries

--- ClassScheduler/src/classscheduler/ClassScheduler.java
+++ ClassScheduler/src/classscheduler/ClassScheduler.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class ClassScheduler {
 
     public static void main(String[] args) {    
-        Queries sq = new Queries();
-        List<Student> students = sq.getAllStudents();
-        List<Course> courses = sq.getAllCourses();
+        Queries q = new Queries();
+        List<Student> students = q.getAllStudents();
+        List<Course> courses = q.getAllCourses();
         System.out.println("Hello World!");
         System.out.println(students.get(0).getfName());
+        System.out.println(courses.get(0).getName());
+        students = q.getByIDNum(910424703);
+        System.out.println(students.get(0).getStudentID());  
+        courses = q.getByCourseName("CMPSC360");
         System.out.println(courses.get(0).getName());
     }
 }
--- ClassScheduler/src/classscheduler/Queries.java
+++ ClassScheduler/src/classscheduler/Queries.java
@@ -7,9 +7,10 @@ import java.util.List;
 public class Queries {
     private Connection conn;
     private PreparedStatement selectAllStudents;
-    private PreparedStatement selectByLName;
+    private PreparedStatement selectByIDNum;
     private PreparedStatement newStudent;
     private PreparedStatement selectAllCourses;
+    private PreparedStatement selectByCourseName;
     private PreparedStatement newCourse;
     
     public Queries() {
@@ -17,12 +18,14 @@ public class Queries {
             conn = Database.getConnection();
             
             selectAllStudents = conn.prepareStatement("SELECT * FROM STUDENTS");
-            selectByLName = conn.prepareStatement("SELECT * FROM STUDENTS WHERE"
-                    + " L_NAME = ?"); 
+            selectByIDNum = conn.prepareStatement("SELECT * FROM STUDENTS WHERE"
+                    + " ID_NUM = ?"); 
             newStudent = conn.prepareStatement("INSERT INTO STUDENTS"
                     + "(ID_NUM, F_NAME, L_NAME, CAMPUS, SEM_STANDING, T_CREDITS)"
                     + "VALUES (?,?,?,?,?,?)");     
             selectAllCourses = conn.prepareStatement("SELECT * FROM COURSES");
+            selectByCourseName = conn.prepareStatement("SELECT * FROM COURSES WHERE"
+                    + " NAME = ?");            
         }
         catch(SQLException sqlException) {
             sqlException.printStackTrace();
@@ -65,13 +68,13 @@ public class Queries {
         return results;
     }
     
-    public List<Student> getByLName(String name) {
+    public List<Student> getByIDNum(int id) {
         List< Student > results = null;
         ResultSet rs = null;
         
         try {
-            selectByLName.setString(1, name);
-            rs = selectByLName.executeQuery();
+            selectByIDNum.setInt(1, id);
+            rs = selectByIDNum.executeQuery();
             results = new ArrayList<Student>();
             
             while(rs.next()) {
@@ -108,6 +111,42 @@ public class Queries {
         
         try {
             rs = selectAllCourses.executeQuery();
+            results = new ArrayList<Course>();
+            
+            while(rs.next()) {
+                results.add(new Course (
+                    rs.getString("NAME"),
+                    rs.getString("CAMPUS"),
+                    rs.getString("SEMESTER"),
+                    rs.getBoolean("IS_FULL"),
+                    rs.getInt("CREDITS"),
+                    rs.getInt("TIME")
+                ));        
+            }
+        }
+        catch (SQLException sqlException) {
+            sqlException.printStackTrace();      
+        }   
+        
+        finally {
+            try {
+                rs.close();
+            }
+            catch(SQLException sqlException) {
+                sqlException.printStackTrace();
+                close();
+            }
+        }
+        return results;
+    }
+    
+    public List<Course> getByCourseName(String name) {
+        List<Course> results = null;
+        ResultSet rs = null;
+        
+        try {
+            selectByCourseName.setString(1, name);
+            rs = selectByCourseName.executeQuery();
             results = new ArrayList<Course>();
             
             while(rs.next()) {
